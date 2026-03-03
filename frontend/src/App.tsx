e05b75ae8a3a527f36085384e1655432daebfaae
Alex <as.voronin777@mail.ru>
2026-03-03 12:26:38 +0300

    added description to frontend file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ interface Item {
   id: number
   type: string
   title: string
-  kind: string
+  description: string
   created_at: string
 }
 
@@ -104,7 +104,7 @@ function App() {
                 <td>{item.id}</td>
                 <td>{item.type}</td>
                 <td>{item.title}</td>
-                <td>{item.kind}</td>
+                <td>{item.description}</td>
                 <td>{item.created_at}</td>
               </tr>
             ))}
